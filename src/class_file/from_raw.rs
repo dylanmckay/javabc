076bd9439a553f23310e54015ed44d5722d2191e
Dylan McKay <dylanmckay34@gmail.com>
2016-12-31 16:17:37 +1300

    Remove all constant-related code from the high-level module

--- src/class_file/from_raw.rs
+++ src/class_file/from_raw.rs
@@ -1,4 +1,4 @@
-use {ClassFile, ConstantPool, Constant, ConstantIndex, Method, Error};
+use {ClassFile, Method, Error};
 use raw;
 
 pub fn from_raw(raw: raw::ClassFile) -> Result<ClassFile, Error> {
@@ -6,7 +6,6 @@ pub fn from_raw(raw: raw::ClassFile) -> Result<ClassFile, Error> {
 
     Ok(ClassFile {
         access_flags: raw.access_flags,
-        constant_pool: self::constant_pool(raw.constant_pool),
         methods: methods,
     })
 }
@@ -22,39 +21,4 @@ fn method(method: &raw::Method,
     Ok(Method {
         name: name,
     })
-}
-
-fn constant_pool(raw: raw::OneBasedArray<raw::Constant, u16>)
-    -> ConstantPool {
-    let mut pool = ConstantPool::new();
-
-    for constant in raw.items {
-        pool.insert(self::constant(constant));
-    }
-
-    pool
-}
-
-fn constant(raw: raw::Constant) -> Constant {
-    match raw {
-        raw::Constant::MethodRef { class, name_and_type } => {
-            Constant::MethodRef { class: ConstantIndex(class.0 as _),
-                                  name_and_type: ConstantIndex(name_and_type.0 as _) }
-        },
-        raw::Constant::InterfaceMethodRef { class, name_and_type } => unimplemented!(),
-        raw::Constant::FieldRef { class, name_and_type } => unimplemented!(),
-        raw::Constant::NameAndType { name, descriptor } => {
-            Constant::NameAndType { name: ConstantIndex(name.0 as _),
-                                    descriptor: ConstantIndex(descriptor.0 as _) }
-        },
-        raw::Constant::Class { name } => {
-            Constant::Class { name: ConstantIndex(name.0 as _) }
-        },
-        raw::Constant::Utf8 { text } => Constant::Utf8(text),
-        raw::Constant::String { index } => unimplemented!(),
-        raw::Constant::Integer(i) => Constant::Integer(i),
-        raw::Constant::Long(l) => unimplemented!(),
-        raw::Constant::Float(f) => unimplemented!(),
-        raw::Constant::Double(d) => unimplemented!(),
-    }
 }
--- src/class_file/mod.rs
+++ src/class_file/mod.rs
@@ -1,31 +1,21 @@
-pub use self::constant::*;
 pub use self::method::Method;
 
-pub mod constant;
 pub mod method;
 
-pub mod as_raw;
 pub mod from_raw;
 
 use Error;
 use raw;
 
-use std::fmt;
-
 #[derive(Clone, Debug)]
 pub struct ClassFile
 {
     pub access_flags: raw::AccessFlags,
-    pub constant_pool: ConstantPool,
     pub methods: Vec<Method>,
 }
 
 impl ClassFile
 {
-    pub fn as_raw(&self) -> ::raw::ClassFile {
-        as_raw::as_raw(self)
-    }
-
     pub fn from_raw(raw: ::raw::ClassFile) -> Result<Self, Error> {
         from_raw::from_raw(raw)
     }
